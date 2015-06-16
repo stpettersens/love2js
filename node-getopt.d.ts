7eeac1c075807ca5fb28664668a029de0c9ab1f0
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-06-16 03:09:24 +0100

    ...

--- node-getopt.d.ts
+++ node-getopt.d.ts
@@ -1,3 +1,8 @@
+// Type definitions for node-getopt
+// Project: https://github.com/jiangmiao/node-getopt
+// Definitions by: Sam Saint-Pettersen <https://github.com/stpettersens>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
 declare module "node-getopt" {
-	export function create(options: string[][]);
+	export function create(options: string[][]): any;
 }
