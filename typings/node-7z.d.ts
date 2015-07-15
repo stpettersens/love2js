f8d9aa1ad8d5effd80fc808f362765cdd1b55b36
Sam Saint-Pettersen <s.stpettersen@gmail.com>
2015-07-15 10:26:06 +0100

    Update node-7z.d.ts

--- typings/node-7z.d.ts
+++ typings/node-7z.d.ts
@@ -4,9 +4,9 @@
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 declare module "node-7z" {
-		export function add(archive: string, files: string[], options?: Object): void;
-		export function extract(archive: string, dest: string, options?: Object): void;
-		export function extractFull(archive: string, dest: string, options?: Object): void;
-		export function list(archive: string, options?: Object): void;
-		export function update(archive: string, files: string[], options?: Object): void;
+	export function add(archive: string, files: string[], options?: Object): void;
+	export function extract(archive: string, dest: string, options?: Object): void;
+	export function extractFull(archive: string, dest: string, options?: Object): void;
+	export function list(archive: string, options?: Object): void;
+	export function update(archive: string, files: string[], options?: Object): void;
 }
